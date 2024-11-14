66b94640c7b0846ea837fdf59220d7aba2b09e3e
braulio2315 <br1412922@gmail.com>
2024-11-13 21:08:49 -0600

    cambios2

---
+++ src/pages/barometer.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function barometer() {
+  return (
+    <div>barometer</div>
+  )
+}
---
+++ src/pages/indicators.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function indicators() {
+  return (
+    <div>indicators</div>
+  )
+}
