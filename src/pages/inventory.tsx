c5d13ca88da5a322901778262ca2d986779fd78e
braulio2315 <br1412922@gmail.com>
2025-05-15 01:31:23 -0600

    ahora si

--- src/pages/inventory.tsx
+++ src/pages/inventory.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useState, useCallback } from 'react';
 import Header from '../components/header';
 import Footer from '../components/piedepagina';
