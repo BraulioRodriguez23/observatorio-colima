c106523092ef3a66e0a255ff86bb34714b5f8d96
braulio2315 <br1412922@gmail.com>
2025-08-07 15:22:58 -0600

    agregacion de logos al header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,45 +1,57 @@
 import React from 'react';
-import logo from '../images/colima-logo.png';
-import logomnz from '../images/Manzanillo -logo.png';
-import logocomala from '../images/Comala Pueblo Mágico.png';
+import logoColima from '../images/colima-logo.png';
+import logoManzanillo from '../images/Manzanillo -logo.png';
+import logoComala from '../images/Comala Pueblo Mágico.png';
 import HorizontalMenu from './Horizontalmenu';
+import udcLogo from '../images/UdeC_DosLineasIzq_Oro.png';
+import gColimaLogo from '../images/Logo SUBSECRETARIA DE TURISMO.png';
 
 const Header: React.FC = () => {
   return (
-    <header className="sticky top-0 w-full bg-white bg-opacity-90 flex flex-wrap justify-between items-center px-4 py-2 shadow-md z-50">
-      
-     <div className="flex items-center space-x-1">
-  {/* Colima – siempre visible en móvil, oculto en ≥sm si quieres */}
-  <img
-    src={logo}
-    alt="Logo Colima"
-    className="h-8 sm:h-10 object-contain sm:hidden"
-  />
-<img
-  src={logomnz}
-  alt="Logo Manzanillo"
-  className="h-2 sm:h-24 object-contain -ml-2 -mr-2"
-/>
+    <header className="sticky top-0 w-full bg-white bg-opacity-90 shadow-md z-50 px-4 py-2">
+      <div className="flex flex-wrap justify-between items-center">
+        {/* Logos */}
+        <div className="flex items-center space-x-3">
+          {/* Logo Colima – visible siempre, cambia tamaño según pantalla */}
+          <img
+            src={logoColima}
+            alt="Logo Colima"
+            className="h-8 sm:h-10 md:h-12 object-contain"
+          />
 
+          {/* Manzanillo – visible en sm+ */}
+          <img
+            src={logoManzanillo}
+            alt="Logo Manzanillo"
+            className="hidden sm:block h-10 md:h-16 object-contain"
+          />
 
-  {/* Colima – vuelve a aparecer en ≥sm */}
-  <img
-    src={logo}
-    alt="Logo Colima"
-    className="hidden sm:block h-9 sm:h-12 object-contain"
-  />
+          {/* Comala – visible en md+ */}
+          <img
+            src={logoComala}
+            alt="Logo Comala"
+            className="hidden md:block h-12 md:h-20 object-contain"
+          />
 
-  {/* Comala – solo en ≥md */}
-  <img
-    src={logocomala}
-    alt="Logo Comala"
-    className="hidden md:block h-12 md:h-20 object-contain"
-  />
-</div>
+          {/* UdeC */}
+          <img
+            src={udcLogo}
+            alt="Logo UdeC"
+            className="hidden sm:block h-10 md:h-10 object-contain"
+          />
 
-      {/* Menú */}
-      <div className="w-full sm:w-auto mt-2 sm:mt-0">
-        <HorizontalMenu />
+          {/* Gobierno de Colima */}
+          <img
+            src={gColimaLogo}
+            alt="Logo Gobierno de Colima"
+            className="hidden md:block h-12 md:h-10 object-contain"
+          />
+        </div>
+
+        {/* Menú */}
+        <div className="w-full sm:w-auto mt-2 sm:mt-0">
+          <HorizontalMenu />
+        </div>
       </div>
     </header>
   );
