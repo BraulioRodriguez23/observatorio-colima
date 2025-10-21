ba674262402dd909481c411ddd946ab5bcab5f9b
braulio2315 <br1412922@gmail.com>
2025-10-21 10:28:59 -0600

    modificacion del disclaimer

--- src/components/disclaimer.tsx
+++ src/components/disclaimer.tsx
@@ -8,7 +8,7 @@ export default function Disclaimer({ onAccept }: DisclaimerProps) {
       <div className="bg-white p-6 rounded-xl shadow-xl max-w-md text-center">
         <h2 className="text-xl font-semibold mb-4 text-black">Aviso importante</h2>
         <p className="mb-4 text-black">
-         La información publicada en el Observatorio Turístico de Colima está sujeta a revisión y actualización, la Subsecretaría de Turismo del Estado de Colima trabaja de manera continua en el cruce de datos, y la verificación de cifras para garantizar su confiabilidad
+        La información publicada en el Observatorio Turístico de Colima está sujeta a revisión y actualización <strong>constante</strong>, la Subsecretaría de Turismo del Estado de Colima trabaja de manera continua en el cruce de datos, y la verificación de cifras para garantizar su confiabilidad.
         </p>
         <button
           className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700"
